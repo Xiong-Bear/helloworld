43cff7b1ac841dd48d11156d1a3c008c16645170
Xiong-Bear <31072312280@qq.com>
2021-10-20 23:03:45 +0800

    20211020 part0 hello world

--- app.rb
+++ app.rb
@@ -2,6 +2,6 @@ require 'sinatra'
 
 class MyApp < Sinatra::Base
   get '/' do
-    '<!DOCTYPE html><html><head></head><body><h1>this is my first sinatra app</h1></body></html>'
+    '<!DOCTYPE html><html><head></head><body><h1>this is xiongyihao first sinatra app</h1></body></html>'
   end
 end
